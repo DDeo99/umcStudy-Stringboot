9c4ecb6e01c953362436626e3bceb3e4fdc529b7
DDeo99 <yoondukwoo@gmail.com>
2023-11-28 23:28:17 +0900

    지역에 가게 추가 기능 생성

---
+++ src/main/java/umcStudy/springStudy/service/StoreService/StoreService.java
@@ -0,0 +1,7 @@
+package umcStudy.springStudy.service.StoreService;
+
+import umcStudy.springStudy.web.dto.request.StoreRequest;
+
+public interface StoreService {
+    void AddStore(StoreRequest.AddStoreDTO addStoreDTO,Long regionId);
+}
