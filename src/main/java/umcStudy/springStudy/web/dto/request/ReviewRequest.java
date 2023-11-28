1cd2ac2539252464f165f4401b024fe382c2aefa
DDeo99 <yoondukwoo@gmail.com>
2023-11-28 23:29:51 +0900

    리뷰 작성 기능 DTO

---
+++ src/main/java/umcStudy/springStudy/web/dto/request/ReviewRequest.java
@@ -0,0 +1,11 @@
+package umcStudy.springStudy.web.dto.request;
+
+import lombok.Getter;
+
+public class ReviewRequest {
+
+    @Getter
+    public static class AddReivewDTO {
+        private String review;
+    }
+}
