320486784b7b0b01ae40a079d3f4e9a9c6637fe0
DDeo99 <yoondukwoo@gmail.com>
2023-11-30 01:29:18 +0900

    가게추가 DTO 검증조건 추가

--- src/main/java/umcStudy/springStudy/web/dto/request/StoreRequest.java
+++ src/main/java/umcStudy/springStudy/web/dto/request/StoreRequest.java
@@ -1,5 +1,8 @@
 package umcStudy.springStudy.web.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import umcStudy.springStudy.domain.Region;
 
@@ -7,7 +10,9 @@ public class StoreRequest {
 
     @Getter
     public static class AddStoreDTO {
+        @NotNull
         private String store_name;
+        @Size(min=5, max=12)
         private String store_address;
     }
 
