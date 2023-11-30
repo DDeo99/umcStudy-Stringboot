4093a0d743aa8728438d4dc65aa29045571b93bf
DDeo99 <yoondukwoo@gmail.com>
2023-12-01 02:09:03 +0900

    store_name에 검증 커스텀 애노테이션

--- src/main/java/umcStudy/springStudy/web/dto/request/StoreRequest.java
+++ src/main/java/umcStudy/springStudy/web/dto/request/StoreRequest.java
@@ -5,12 +5,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import umcStudy.springStudy.domain.Region;
+import umcStudy.springStudy.validation.annotation.ExistCategories;
+import umcStudy.springStudy.validation.annotation.ExistStore;
 
 public class StoreRequest {
 
     @Getter
     public static class AddStoreDTO {
         @NotNull
+        @ExistStore
         private String store_name;
         @Size(min=5, max=12)
         private String store_address;
