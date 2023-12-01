8b4486f6701a99c3be808c9eea2072a46c700bdc
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 01:09:15 +0900

    제목, 스코어 추가

--- src/main/java/umcStudy/springStudy/web/dto/request/ReviewRequest.java
+++ src/main/java/umcStudy/springStudy/web/dto/request/ReviewRequest.java
@@ -1,6 +1,9 @@
 package umcStudy.springStudy.web.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 public class ReviewRequest {
@@ -8,6 +11,11 @@ public class ReviewRequest {
     @Getter
     public static class AddReivewDTO {
         @NotNull
-        private String review;
+        private String reviewHead;
+        @NotNull
+        private String reviewBody;
+        @NotNull
+        @Min(0)@Max(5)
+        private float score;
     }
 }
