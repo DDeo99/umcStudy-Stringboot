db96b9cf398012872e240eaf6f6f3ed6eebf0dde
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 02:39:33 +0900

    해당 멤버의 리뷰 리스트 조회 추가

--- src/main/java/umcStudy/springStudy/service/MemberService/MemberCommnandServiceImpl.java
+++ src/main/java/umcStudy/springStudy/service/MemberService/MemberCommnandServiceImpl.java
@@ -1,6 +1,10 @@
 package umcStudy.springStudy.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.webjars.NotFoundException;
@@ -9,11 +13,15 @@ import umcStudy.springStudy.apiPayload.code.status.ErrorStatus;
 import umcStudy.springStudy.apiPayload.exception.handler.FoodCategoryHandler;
 import umcStudy.springStudy.converter.MemberConverter;
 import umcStudy.springStudy.converter.MemberPreferConverter;
+import umcStudy.springStudy.converter.ReviewConverter;
 import umcStudy.springStudy.domain.FoodCategory;
 import umcStudy.springStudy.domain.Mapping.MemberPrefer;
 import umcStudy.springStudy.domain.Member;
+import umcStudy.springStudy.domain.Review;
+import umcStudy.springStudy.domain.Store;
 import umcStudy.springStudy.repository.FoodCategoryRepository;
 import umcStudy.springStudy.repository.MemberRepository;
+import umcStudy.springStudy.repository.ReviewRepository;
 import umcStudy.springStudy.web.dto.request.MemberRequestDTO;
 
 import java.util.List;
@@ -25,7 +33,7 @@ import java.util.stream.Collectors;
 public class MemberCommnandServiceImpl implements MemberCommandService{
     private final MemberRepository memberRepository;
     private final FoodCategoryRepository foodCategoryRepository;
-
+    private final ReviewRepository reviewRepository;
     @Override
     @Transactional
     public Member joinMember(MemberRequestDTO.JoinDto request) {
@@ -48,5 +56,16 @@ public class MemberCommnandServiceImpl implements MemberCommandService{
                 .orElseThrow(()->new NotFoundException("해당하는 유저가 없습니다."));
 
         return member;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long MemberId, Integer page) {
+
+        Member member = memberRepository.findById(MemberId).get();
+
+        // 회원의 리뷰 리스트 가져오기
+        Page<Review> reviewPage = reviewRepository.findByMember(member, PageRequest.of(page, 10));
+
+        return reviewPage;
     }
 }
