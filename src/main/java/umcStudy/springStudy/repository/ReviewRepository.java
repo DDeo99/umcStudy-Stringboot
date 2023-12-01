d4ceed3299a08e9663c25213295e68d7359157bf
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 01:45:46 +0900

    findAllByStore 추가

--- src/main/java/umcStudy/springStudy/repository/ReviewRepository.java
+++ src/main/java/umcStudy/springStudy/repository/ReviewRepository.java
@@ -1,9 +1,14 @@
 package umcStudy.springStudy.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import umcStudy.springStudy.domain.Review;
+import umcStudy.springStudy.domain.Store;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
 }
