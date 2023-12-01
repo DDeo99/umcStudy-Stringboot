a2eae2b9571114627be9fa064e300931e4e6661f
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 01:45:25 +0900

    해당 가게의 리뷰 리스트 컨버터 추가

--- src/main/java/umcStudy/springStudy/converter/ReviewConverter.java
+++ src/main/java/umcStudy/springStudy/converter/ReviewConverter.java
@@ -1,8 +1,14 @@
 package umcStudy.springStudy.converter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import umcStudy.springStudy.domain.Review;
 import umcStudy.springStudy.web.dto.request.ReviewRequest;
+import umcStudy.springStudy.web.dto.response.StoreResponseDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReviewConverter {
 
@@ -13,4 +19,39 @@ public class ReviewConverter {
                 .score(request.getScore())
                 .build();
     }
+
+    public static StoreResponseDTO.CreateReviewResultDTO toCreateReviewResultDTO(Review review){
+        return StoreResponseDTO.CreateReviewResultDTO.builder()
+                .reviewId(review.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(ReviewConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(List<Review> reviewList){
+        return null;
+    }
+
+
 }
