e5767dfaa12c18d74b6b2864e52613c2a2828c9e
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 17:27:07 +0900

    가게의 미션 리스트 조회 기능

--- src/main/java/umcStudy/springStudy/service/StoreService/StoreService.java
+++ src/main/java/umcStudy/springStudy/service/StoreService/StoreService.java
@@ -1,9 +1,13 @@
 package umcStudy.springStudy.service.StoreService;
 
+import org.springframework.data.domain.Page;
+import umcStudy.springStudy.domain.Mission;
 import umcStudy.springStudy.web.dto.request.StoreRequest;
 
 public interface StoreService {
     void AddStore(StoreRequest.AddStoreDTO addStoreDTO,Long regionId);
 
     boolean existsByName(String name);
+
+    Page<Mission> getMissionList(Long storeId, Integer page);
 }
