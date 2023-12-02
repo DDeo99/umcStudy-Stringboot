8dec759a9e5a185a76601f441d2d3fd20e3ddfa4
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 16:40:52 +0900

    isValid 변수 사용하여 코드 포맷 맞춤

--- src/main/java/umcStudy/springStudy/validation/validator/PagingValidator.java
+++ src/main/java/umcStudy/springStudy/validation/validator/PagingValidator.java
@@ -2,9 +2,12 @@ package umcStudy.springStudy.validation.validator;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+import umcStudy.springStudy.apiPayload.code.status.ErrorStatus;
 import umcStudy.springStudy.validation.annotation.CheckMissionStatus;
 import umcStudy.springStudy.validation.annotation.CheckPaging;
 
+@Component
 public class PagingValidator implements ConstraintValidator<CheckPaging, Integer> {
     @Override
     public void initialize(CheckPaging constraintAnnotation) {
@@ -14,6 +17,13 @@ public class PagingValidator implements ConstraintValidator<CheckPaging, Integer
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
 
-        return value!=null && value >=1;
+        boolean isValid = value!=null && value >=1;
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.PAGING_BAD_REQUEST.toString()).addConstraintViolation();
+        }
+
+        return isValid;
     }
 }
