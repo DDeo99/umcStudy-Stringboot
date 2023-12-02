22833b2365cb3ac9b454cbedea659253f4b61754
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 22:44:59 +0900

    미션 Response 변환

--- src/main/java/umcStudy/springStudy/converter/MissionConverter.java
+++ src/main/java/umcStudy/springStudy/converter/MissionConverter.java
@@ -1,9 +1,16 @@
 package umcStudy.springStudy.converter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import umcStudy.springStudy.domain.Mission;
 import umcStudy.springStudy.repository.MissionRepository;
 import umcStudy.springStudy.web.dto.request.MissionRequest;
+import umcStudy.springStudy.web.dto.response.MissionResponseDTO;
+import umcStudy.springStudy.web.dto.response.StoreResponseDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MissionConverter {
 
@@ -12,6 +19,36 @@ public class MissionConverter {
                 .deadline(request.getDeadline())
                 .mission_spec(request.getMission_spec())
                 .reward(request.getReward())
+                .build();
+    }
+
+    public static MissionResponseDTO.CreateMissionResultDTO toCreateMissionResultDTO(Mission mission){
+        return MissionResponseDTO.CreateMissionResultDTO.builder()
+                .missionId(mission.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static MissionResponseDTO.MissionDTO missionDTO(Mission mission){
+        return MissionResponseDTO.MissionDTO.builder()
+                .missionSpec(mission.getMission_spec())
+                .reward(mission.getReward())
+                .createdAt(mission.getCreatedAt().toLocalDate())
+                .deadLine(mission.getCreatedAt().toLocalDate())
+                .build();
+    }
+    public static MissionResponseDTO.MissionListDTO missionListDTO(Page<Mission> missionList){
+        List<MissionResponseDTO.MissionDTO> missionDTOList = missionList.stream()
+                .map(MissionConverter::missionDTO)
+                .collect(Collectors.toList());
+
+        return MissionResponseDTO.MissionListDTO.builder()
+                .isLast(missionList.isLast())
+                .isFirst(missionList.isFirst())
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .listSize(missionDTOList.size())
+                .missionList(missionDTOList)
                 .build();
     }
 }
