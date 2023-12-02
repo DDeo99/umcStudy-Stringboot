99f6f4ed3a28ea05bab7cea21a246a1384a4586d
DDeo99 <yoondukwoo@gmail.com>
2023-12-02 22:45:18 +0900

    진행중인 미션 리스트 조회

--- src/main/java/umcStudy/springStudy/web/controller/MissionRestController.java
+++ src/main/java/umcStudy/springStudy/web/controller/MissionRestController.java
@@ -2,10 +2,18 @@ package umcStudy.springStudy.web.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umcStudy.springStudy.apiPayload.ApiResponse;
+import umcStudy.springStudy.converter.MissionConverter;
+import umcStudy.springStudy.converter.ReviewConverter;
+import umcStudy.springStudy.domain.Mission;
+import umcStudy.springStudy.domain.Review;
 import umcStudy.springStudy.service.MissionService.MissionService;
+import umcStudy.springStudy.validation.annotation.CheckPaging;
 import umcStudy.springStudy.web.dto.request.MissionRequest;
+import umcStudy.springStudy.web.dto.response.MemberResponseDTO;
+import umcStudy.springStudy.web.dto.response.MissionResponseDTO;
 
 @RestController
 @RequestMapping("/missions")
@@ -21,6 +29,16 @@ public class MissionRestController {
         missionService.AddMission(request, storeId);
 
         return ApiResponse.onSuccess(null);
+    }
+
+    @GetMapping("/members/{memberId}")
+
+    public ApiResponse<MissionResponseDTO.MissionListDTO> getMyMissionList(@PathVariable("memberId") Long memberId,
+                                                                           @RequestParam(name="page") @CheckPaging Integer page) {
+
+        Page<Mission> missionPage = missionService.getMyMissionList(memberId, page-1);
+        MissionResponseDTO.MissionListDTO missionListDTO = MissionConverter.missionListDTO(missionPage);
+        return ApiResponse.onSuccess(missionListDTO);
     }
 
 }
